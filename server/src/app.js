ae38449e7efb96a193904c3fec0d19bd1ac8d452
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-05-23 15:23:13 +0600

    rebase

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.status(200).json({message: 'Hello, world'});
+});
+
+module.exports.app = app;
