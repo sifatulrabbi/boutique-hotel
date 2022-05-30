fac7c57b09244e7409141fa68bd28e10d1575570
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-05-30 18:41:06 +0600

    feat: add about page

---
+++ web/src/pages/AboutPage/Carousal.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Carousal = () => {
+  return <div>Carousal</div>;
+};
+
+export default Carousal;
---
+++ web/src/pages/AboutPage/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PageMain from '../../components/PageMain';
+
+const AboutPage = () => {
+  return <PageMain>AboutPage</PageMain>;
+};
+
+export default AboutPage;
