4eebd01274df5086eb2f3d11dfc9ff6a64853f69
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-06-28 17:43:54 +0600

    fix: Single day detection

--- server/src/utils.js
+++ server/src/utils.js
@@ -6,6 +6,14 @@ module.exports.calcTimeDifference = function (start, end) {
   const startTime = start.getTime();
   const endTime = end.getTime();
 
+  if (startTime === endTime) {
+    return {
+      days: 1,
+      hours: 24,
+      minutes: 24 * 60,
+    };
+  }
+
   const timeDifference = Math.abs(endTime - startTime);
   const minutes = timeDifference / 1000 / 60;
   const hours = minutes / 60;
